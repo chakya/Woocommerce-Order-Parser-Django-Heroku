afa173a66fae3f6234e92c87ab4026b76b8ec2a3
Charles Wijaya <charleswijaya@Charless-MacBook-Pro-2.local>
2017-05-11 23:21:40 +0800

    make it better

--- hook/views.py
+++ hook/views.py
@@ -9,30 +9,31 @@ import requests
 @csrf_exempt
 @require_POST
 def webhook(request):
-    jsondata = request.body
-    # print(jsondata)
-    data = json.loads(jsondata.decode())
-    # print(data)
-    orderItem=data['order']['line_items'][0]
-    orderName=orderItem['name']
-    orderPrice=orderItem['price']
-    orderQuatity=orderItem['quantity']
-    customer=data['order']['customer']
-    custAdd=customer['billing_address']
-    address=custAdd['address_1']+custAdd['address_2']
+    return 200
+    # jsondata = request.body
+    # # print(jsondata)
+    # data = json.loads(jsondata.decode())
+    # # print(data)
+    # orderItem=data['order']['line_items'][0]
+    # orderName=orderItem['name']
+    # orderPrice=orderItem['price']
+    # orderQuatity=orderItem['quantity']
+    # customer=data['order']['customer']
+    # custAdd=customer['billing_address']
+    # address=custAdd['address_1']+custAdd['address_2']
 
-    text='{} {} {} {}'.format(orderQuatity,orderName, orderPrice, address)
+    # text='{} {} {} {}'.format(orderQuatity,orderName, orderPrice, address)
 
 
-    #send to telegram
-    url='https://api.telegram.org/bot'
-    url+='271450263:AAHV8VK5b1HiZUGz_cLp5bmd3epalHsIz3c/'
-    sendMesUrl=url+'sendMessage?chat_id={}&text={}'
-    requests.post(sendMesUrl.format("269856018",text))
-    requests.post(sendMesUrl.format("235092071", text))
-    requests.post(sendMesUrl.format("329891854", text))
-    print(data['order']['line_items'][0]['name'])
-    return HttpResponse(status=200)
+    # #send to telegram
+    # url='https://api.telegram.org/bot'
+    # url+='271450263:AAHV8VK5b1HiZUGz_cLp5bmd3epalHsIz3c/'
+    # sendMesUrl=url+'sendMessage?chat_id={}&text={}'
+    # requests.post(sendMesUrl.format("269856018",text))
+    # requests.post(sendMesUrl.format("235092071", text))
+    # requests.post(sendMesUrl.format("329891854", text))
+    # print(data['order']['line_items'][0]['name'])
+    # return HttpResponse(status=200)
     # WebhookTransaction.objects.create(
     #     date_event_generated=datetime.datetime.fromtimestamp(
     #         data['timestamp']/1000.0,
